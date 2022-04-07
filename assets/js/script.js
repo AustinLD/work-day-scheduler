4e1b42f1bd1a53ac10c73add9bdd9e9f955e0296
Austin Donovan <AustinLDonovan@gmail.com>
2022-04-07 01:02:25 -0400

    code is completed and functioning need to comment style and add readme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,137 +1,4 @@
-var timeofDay = document.querySelector("#currentDay")
-const entries = document.getElementsByClassName("information");
-
-
-
-var currentHour = moment().hour();
-var currentDay = moment().day();
-
-var currentTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-timeofDay.textContent= currentTime;
-
-
-var update = function() {
-  timeofDay.innerHTML = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-}; setInterval(update, 1000);
-
-
-
-
-
-
-
-Array.from(entries).forEach(information => {
-  let
-    rowIdString = information.id,
-    rowHour;
-  if (rowIdString) {
-    rowHour = parseInt(rowIdString);
-  }
-  if (rowHour) {
-    if (currentHour === rowHour) {
-      row.addclass(".present");
-    } else if ((currentHour < rowHour) && (currentHour > rowHour - 6)) {
-      setColor(row, "green");
-    } else if ((currentHour > rowHour) && (currentHour < rowHour + 6)) {
-      setColor(row, "lightgrey");
-    } else {
-      setColor(row, "white");
-    }
-  }
-});
-
-function setColor(element, color) {
-  element.style.backgroundColor = color;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-$(document).ready(function() {
-  $(".saveBtn").on("click", function() {
-    var value = $(this).siblings(".information").val()
-    var time = $(this).parent().attr("id");
-    localStorage.setItem(time, value);
-    localStorage.getItem(time, value);
-  })
-  function colorUpdater() {
-    Array.from(entries).forEach(information => {
-      let
-        rowIdString = information.id,
-        rowHour;
-      if (rowIdString) {
-        rowHour = parseInt(rowIdString);
-      }
-      if (rowHour) {
-        if (currentHour === rowHour) {
-          entries.classlist.add("present");
-        } else if ((currentHour < rowHour) && (currentHour > rowHour - 6)) {
-          setColor(row, "green");
-        } else if ((currentHour > rowHour) && (currentHour < rowHour + 6)) {
-          setColor(row, "lightgrey");
-        } else {
-          setColor(row, "white");
-        }
-      }
-    });
-  }
-
-
-})
-
-
-
-// function colorUpdater() {
-//   Array.from(entries).forEach(information => {
-//     let
-//       rowIdString = information.id,
-//       rowHour;
-//     if (rowIdString) {
-//       rowHour = parseInt(rowIdString);
-//     }
-//     if (rowHour) {
-//       if (currentHour === rowHour) {
-//         row.addclass(".present");
-//       } else if ((currentHour < rowHour) && (currentHour > rowHour - 6)) {
-//         setColor(row, "green");
-//       } else if ((currentHour > rowHour) && (currentHour < rowHour + 6)) {
-//         setColor(row, "lightgrey");
-//       } else {
-//         setColor(row, "white");
-//       }
-//     }
-//   });
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// populates value of .information on page from local storage
 $("#9 .information").val(localStorage.getItem("9"));
 $("#10 .information").val(localStorage.getItem("10"));
 $("#11 .information").val(localStorage.getItem("11"));
@@ -142,14 +9,53 @@ $("#15 .information").val(localStorage.getItem("15"));
 $("#16 .information").val(localStorage.getItem("16"));
 $("#17 .information").val(localStorage.getItem("17"));
 
-// $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+//targets currentDay ID in HTML
+var timeofDay = document.querySelector("#currentDay")
+
+//targets ALL divs with row class in HTML
+const entries = document.querySelectorAll(".row");
+
+// variables equal to current hour / day
+var currentHour = moment().hour();
+var currentDay = moment().day();
+
+// formats the date and time on the top of the page
+var currentTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+timeofDay.textContent= currentTime;
+
+// updates the date on the top of the page automatically.
+var update = function() {
+  timeofDay.innerHTML = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+}; setInterval(update, 1000);
 
 
+// function executes as soon as page loads
+$(document).ready(function() {
+  $(".saveBtn").on("click", function() { // listens to saveBtn for on click
+    var value = $(this).siblings(".information").val() // variable storing value of all siblings of .saveBtn with .information class
+    var time = $(this).parent().attr("id"); // stores ID attribute of parent div of .saveBtn
+    localStorage.setItem(time, value); // stores time and value variables in local storage
+  })
+  colorUpdater() // calls color updater function
+});
 
 
-
-
-
-
-
-// // var nterval = setInterval(colorUpdater, 15000);
+// changes color of .row based on time of day relative to the rows dedicated time
+function colorUpdater() {
+  Array.from(entries).forEach(row => { // calls the function below for each .row inside of entries. entries = .row
+    let
+    rowIdString = row.id // 
+    if (rowIdString) {
+      rowHour = parseInt(rowIdString);
+    }
+    if (rowHour) {
+      if (currentHour === rowHour) {
+        $(".information").addClass("present");
+      } else if (currentHour < rowHour) {
+        $(".information").addClass("future");
+      } else if (currentHour > rowHour) {
+        $(".information").addClass("past");
+      }
+    }
+  });
+}
